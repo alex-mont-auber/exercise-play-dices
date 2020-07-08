e8b159424a71f85e3b46b7e8a5d4d209f806d153
Alexandre <amontauber@gmail.com>
2020-07-08 13:59:54 +0200

    First version of Front end. With POST Player working.

---
+++ XAMPP/js/script.js
@@ -0,0 +1,94 @@
+// ** -- BASE STRUCTURE -- **\\
+
+function openTab(evt, cityName) 
+{
+
+    // Variables
+
+  var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) 
+  {
+    tabcontent[i].style.display = "none";
+  }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) 
+  {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+
+  document.getElementById(cityName).style.display = "block";
+  
+  evt.currentTarget.className += " active";
+} 
+
+
+// ** -- AJAX Dice Games -- ** \\
+
+    // ** -- URL BASE
+
+const url = "http://localhost:8080";
+
+
+    // ** -- POST
+
+function createPlayer() 
+{
+    //let actualDate = new Date();
+
+    let takeInfo = 
+    {
+        idPlayer: "",
+        playerName: $("#inputName").val(),
+        perCentSuccess: 0,
+        numberGames:0,
+        dateRegistrer: "",
+    }
+
+    $.ajax({
+        type:'POST',
+        url: url + '/players',
+        contentType: 'application/json',
+        data: JSON.stringify(takeInfo),
+        success: function()
+        {
+          document.getElementById("presentationBox").innerHTML = "";
+          document.getElementById("presentationBox").innerHTML = "<p>The registration is correct! Please wait...</p>";
+          function timeWait() {
+            //  setTimeout("", 5000);
+             // document.getElementById("presentationBox").innerHTML ="<button>"
+            }
+          //window.location = "https://localhost/diceGames/selectGame.html";
+        },
+        error: function() 
+        {
+            document.getElementById("presentationBox").innerHTML = "";
+            document.getElementById("presentationBox").innerHTML = "<p>The registration was not successful! Please try again...</p>";
+            setTimeout("", 5000);
+            location.reload();
+        }
+    })
+}
+
+    //  ** -- GETS 
+function showRanking() {
+    $.ajax({
+        type: 'GET',
+        url: url + '/players',
+        contentType: 'application/json',
+        success: function()
+        {
+    
+        }
+    })
+}
